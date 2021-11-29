32ed67a7c19b813441810cd31ac4784c73c0b783
Michael Wagoner <mwwagoner@gmail.com>
2021-11-28 20:58:15 -0500

    Adds some comments to nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,10 +2,19 @@ const content = `
         <li><a href="index.html">Home</a></li>
         <li><a href="tricube.html">Tricube Tales</a></li>
 `
-
-const defaultHeader = document.querySelector("header");
+/* Create the new nav and ul elements for the nav bar */
 const newNaviBar = document.createElement("nav")
 const navBarList = document.createElement("ul");
+
+/* Grab the header element from the DOM */
+const defaultHeader = document.querySelector("header");
+
+/* Add the class attribute to the new nav element */
+newNaviBar.setAttribute("class", "naviBar");
+
+/* Set the ul HTML to the li elements in the nav bar */
 navBarList.innerHTML = content;
+
+/* Add the ul to the nav element and then add the nav element to the header */
 newNaviBar.append(navBarList);
 defaultHeader.append(newNaviBar);
