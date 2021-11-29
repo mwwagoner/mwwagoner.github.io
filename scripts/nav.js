d6537821bbe9a2cf4c8edbd04e3cfc165aa90879
Michael Wagoner <mwwagoner@gmail.com>
2021-11-28 21:23:47 -0500

    Changes the class attribute for the nav element to navi-bar for consistency

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,6 @@
 const content = `
-        <li><a href="index.html">Home</a></li>
-        <li><a href="tricube.html">Tricube Tales</a></li>
+        <li class="nav-list-item"><a href="index.html">Home</a></li>
+        <li class="nav-list-item"><a href="tricube.html">Tricube Tales</a></li>
 `
 /* Create the new nav and ul elements for the nav bar */
 const newNaviBar = document.createElement("nav")
@@ -10,7 +10,7 @@ const navBarList = document.createElement("ul");
 const defaultHeader = document.querySelector("header");
 
 /* Add the class attribute to the new nav element */
-newNaviBar.setAttribute("class", "naviBar");
+newNaviBar.setAttribute("class", "navi-bar");
 
 /* Set the ul HTML to the li elements in the nav bar */
 navBarList.innerHTML = content;
